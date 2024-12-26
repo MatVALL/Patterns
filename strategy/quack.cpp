53f7895839c8cdf3b1499fb09f78dce5f3e1a610
MaVaL <mathieu_vallet@orange.fr>
2024-12-26 02:04:05 +0100

    strategy: add quack behaviour

--- strategy/quack.cpp
+++ strategy/quack.cpp
@@ -1,1 +1,12 @@
 #include "quack.h"
+
+/* {{{ MuteQuack*/
+void MuteQuack::quack() {
+    std::cout << "..." << std::endl;
+}
+/* }}} */
+/* {{{ Quack */
+void Quack::quack() {
+    std::cout << "quack quack" << std::endl;
+}
+/* }}} */
--- strategy/quack.h
+++ strategy/quack.h
@@ -1,1 +1,14 @@
+#include <iostream>
 
+class QuackBehaviour {
+    public:
+        virtual void quack() = 0;
+};
+
+class Quack: public QuackBehaviour {
+    void quack();
+};
+
+class MuteQuack: public QuackBehaviour{
+    void quack();
+};
