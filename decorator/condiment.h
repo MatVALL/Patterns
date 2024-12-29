6cf6fdb96204d232a84e168ccffc9bbcb53b23a5
MaVaL <mathieu_vallet@orange.fr>
2024-12-29 13:34:45 +0100

    add base beverages

--- decorator/condiment.h
+++ decorator/condiment.h
@@ -4,6 +4,31 @@
 #include "beverage.h"
 
 class Espresso: public Beverage {
+    public:
+        Espresso() = default;
+        double cost();
+        std::string getDescription ();
 };
 
+double Espresso::cost() {
+    return 5.99; // it is StarBuzz after all
+}
+std::string Espresso::getDescription() {
+    return "Espresso\t" + std::to_string(cost());
+}
+
+class Tea: public Beverage {
+    public:
+        Tea() = default;
+        double cost();
+        std::string getDescription ();
+};
+
+double Tea::cost() {
+    return 3.99; // it is StarBuzz after all
+}
+
+std::string Tea::getDescription() {
+    return "Tea\t " + std::to_string(cost());
+}
 #endif
